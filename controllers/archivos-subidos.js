a472319705878e9ac0a7f83e2520d0b5d7ac0670
Johxn <71521703+Johxn@users.noreply.github.com>
2022-04-01 15:33:47 -0600

    terminando archivos

--- controllers/archivos-subidos.js
+++ controllers/archivos-subidos.js
@@ -3,12 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
     var instances = M.Sidenav.init(elems, options);
 });
 
+/*Boton de ir hacia arriba*/
+
+var hastatop = document.getElementById("hasta_arriba");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        hastatop.style.display = "block";
+    } else {
+        hastatop.style.display = "none";
+    }
+};
+
+hastatop.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
 // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
 // Or with jQuery
-
-$(document).ready(function () {
-    $('.sidenav').sidenav();
-});
