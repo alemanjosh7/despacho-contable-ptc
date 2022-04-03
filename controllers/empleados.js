a5d94ea5184b7bff77afa3f00c59bc6586ee02fa
alemanjosh7 <josuealeman2003@gmail.com>
2022-04-03 13:09:50 -0600

    terminando view de archivos y empleados con todo y modals de eliminar, editar y añadir

--- controllers/empleados.js
+++ controllers/empleados.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'bottom',
-    });
+const btnPuntitos = document.getElementById('btn-puntitos');
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'bottom',
+
   });
+});
 
 /*Boton de ir hacia arriba*/
 
@@ -26,8 +29,10 @@ hastatop.addEventListener("click", function () {
   });
 });
 
+
+
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
   // var collapsibleElem = document.querySelector('.collapsible');
   // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
-  // Or with jQuery
+
