c18f6f6a1217c953bf657d47922dd313b51a5ec3
JonathanGdR <85887616+JonathanGdR@users.noreply.github.com>
2022-06-15 10:35:56 -0600

    Add files via upload

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,18 @@
+// Constante para establecer la ruta y parámetros de comunicación con la API.
+const API_EMPLEADOS = SERVER + 'dashboard/empleados.php?action=';
+const API_GLBVAR = SERVER + 'variablesgb.php?action=';
+
 //Inicializando componentes de Materialize
 document.addEventListener('DOMContentLoaded', function () {
     M.Sidenav.init(document.querySelectorAll('.sidenav'));
     M.Tooltip.init(document.querySelectorAll('.tooltipped'));
 });
 //Inputs
-const contraInput = document.getElementById('contraseña');
-const mostrarOcultarIconPass = document.getElementById('ocultarmostrar_contraseña');
+const LOGINBTN = document.getElementById('Iniciar_btn');//Boton de inicio de sesión
+const USUARIOTXT = document.getElementById('username');//input del nombre del usuario
+const CONTRAINPUT = document.getElementById('contraseña');
+const OJOICON = document.getElementById('ocultarmostrar_contraseña');
+
 ///*Boton de ir hacia arrina*/
 var hastatop = document.getElementById('hasta_arriba');
 window.onscroll = function(){
@@ -23,12 +30,46 @@ hastatop.addEventListener('click', function(){
     })
 });
 //Visualizar u ocultar contraseña
-mostrarOcultarIconPass.addEventListener('click',function(){
-    if (contraInput.type == "password") {
-        contraInput.type = "text"
-        mostrarOcultarIconPass.innerText = "visibility_off"
+OJOICON.addEventListener('click',function(){
+    if (CONTRAINPUT.type == "password") {
+        CONTRAINPUT.type = "text"
+        OJOICON.innerText = "visibility_off"
     } else {
-        contraInput.type = "password"
-        mostrarOcultarIconPass.innerText = "visibility"
+        CONTRAINPUT.type = "password"
+        OJOICON.innerText = "visibility"
+    }
+});
+
+
+
+
+
+//Función de log in
+LOGINBTN.addEventListener('click', function () {
+    if (USUARIOTXT.value.length > 0 && CONTRAINPUT.value.length > 0) {
+        LOGINBTN.classList.add('disabled');
+        fetch(API_EMPLEADOS + 'logIn', {
+            method: 'post',
+            body: new FormData(document.getElementById('session-form'))
+        }).then(function (request) {
+            // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje en la consola indicando el problema.
+            if (request.ok) {
+                request.json().then(function (response) {
+                    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                    if (response.status) {
+                        sweetAlert(1, response.message, 'inicio.html');
+                        LOGINBTN.classList.remove('disabled');
+                    } else {
+                        LOGINBTN.classList.remove('disabled');
+                        sweetAlert(2, response.exception, null);
+                    }
+                });
+            } else {
+                LOGINBTN.classList.remove('disabled');
+                console.log(request.status + ' ' + request.statusText);
+            }
+        });
+    } else {
+        sweetAlert(3, 'Debe de completar el formulario para iniciar sesion', null);
     }
 });
