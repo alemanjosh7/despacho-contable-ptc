65f6c3476ee352e66bea824811acfa00472f85b2
JonathanGdR <85887616+JonathanGdR@users.noreply.github.com>
2022-09-02 09:54:04 -0600

    Update enviarCorreoI.php
    Diseño de correo

--- api/enviarCorreoI.php
+++ api/enviarCorreoI.php
@@ -31,6 +31,8 @@ class EnvioC
         $body = '<!DOCTYPE html
         PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+        
+        
         <head>
         <!--Import Google Icon Font-->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -48,11 +50,27 @@ class EnvioC
             body{
                 background-color: #F1F5F9;
             }
+
+            .fondo-0{
+            padding: 20px;
+            background-color: #dee8f1;
+            text-align: center;
+            /*margin: 100px;*//*borrar linea*/
+            }
+            .fondo {
+                padding: 40px;
+                background-color: #F1F5F9;
+                text-align: center;
+                box-shadow: 3px 3px 4px 2px rgb(138, 136, 136);
+            }
             .encabezado{
                 width: 100%;
                 height: 100px;
                 background-color: #F4A172;
                 text-align: center;
+                border-radius: 5px;
+                font-family: '.'Poppins'.', sans-serif;
+                padding-top: 10px;
             }
             
             .fila1 {
@@ -66,28 +84,46 @@ class EnvioC
                 height: 100px;
                 border-radius: 10px;
             }
+
+            .container h1{
+                font-family: '.'Poppins'.', sans-serif;
+                font-size: 25px;
+            }
+            
+            .container p{
+                font-family: '.'Poppins'.', sans-serif;
+                font-size: 14px;
+            }
+
             .card-panel h1{
                 margin-left: 40%;
                 padding-top: 25px;
+                font-family: '.'Poppins'.', sans-serif;
             }
             </style>
             </head>
         </head>
         <body class=" grey lighten-3" style="margin: 0; padding:0;">
-        <div class="encabezado">
-            <nav class="orange darken-4 center">
-                <div class="nav-wrapper">
-                    <h5 style="margin: 0; font-size: 50px;">Smart Book</h5>
+        <div class="fondo-0">
+            <div class="edge">
+                <div class="fondo">
+                    <div class="encabezado">
+                        <nav class="orange darken-4 center">
+                            <div class="nav-wrapper">
+                                <h5 style="margin: 0; font-size: 50px;">Smart Book</h5>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="container">
+                        <h1 class="center-align" style="text-align: center;  margin-top: 1%;">¡Se bloqueo un usuario!</h1>
+                        <br>
+                        <p  style="text-align: center;">Te informamos que el usuario :'.$usuario.', perteneciente al empleado: '.$nombreCE.'. ¡Ha sido bloqueado!</p>
+                        <p style="text-align: center;"><b>Para desbloquearlo dirigirse a la aplicación; Apartado empleado->Modificar al empleado bloqueado</b></p>
+                        <br>
+                        <p style="text-align: center;">Cambie su estado a activo para desbloquearlo</p>
+                    </div>
                 </div>
-            </nav>
-        </div>
-        <div class="container">
-            <h1 class="center-align" style="text-align: center;  margin-top: 1%;">¡Se bloqueo un usuario!</h1>
-            <br>
-            <p  style="text-align: center;">Te informamos que el usuario :'.$usuario.', perteneciente al empleado: '.$nombreCE.'. ¡Ha sido bloqueado!</p>
-            <p style="text-align: center;"><b>Para desbloquearlo dirigirse a la aplicación; Apartado empleado->Modificar al empleado bloqueado</b></p>
-            <br>
-            <p style="text-align: center;">Cambie su estado a activo para desbloquearlo</p>
+            </div>
         </div>
         <!--JavaScript al final para optimizar-->
         <!-- Compiled and minified JavaScript -->
