35dd35396e2ddd2dd44e813d51a85e6f258dc2ca
JonathanGdR <85887616+JonathanGdR@users.noreply.github.com>
2022-09-04 11:01:36 -0600

    Update error404.js

--- controllers/error404.js
+++ controllers/error404.js
@@ -2,6 +2,8 @@
 let t1 = gsap.timeline();
 let t2 = gsap.timeline();
 let t3 = gsap.timeline();
+let t4 = gsap.timeline();
+let t5 = gsap.timeline();
 
 
 t1.to(".cog1",
@@ -10,7 +12,7 @@ t1.to(".cog1",
   rotation:"+=360",
   repeat:-1,
   ease:Linear.easeNone,
-  duration:8
+  duration:20
 });
 
 t2.to(".cog2",
@@ -19,7 +21,7 @@ t2.to(".cog2",
   rotation:"-=360",
   repeat:-1,
   ease:Linear.easeNone,
-  duration:8
+  duration:20
 });
 
 t3.fromTo(".wrong-para",
@@ -28,9 +30,36 @@ t3.fromTo(".wrong-para",
 },
 {
   opacity:1,
-  duration:1,
+  duration:3,
   stagger:{
     repeat:-1,
+    yoyo:true
+  }
+});
+
+t4.fromTo(".wrong-para2",
+{
+  opacity:0
+},
+{
+  opacity:1,
+  duration:3,
+  stagger:{
+    repeat:-1,
+    yoyo:true
+  }
+});
+
+
+t5.fromTo(".RegresarFx",
+{
+  opacity:0
+},
+{
+  opacity:1,
+  duration:7,
+  stagger:{
+    repeat:0,
     yoyo:true
   }
 });
