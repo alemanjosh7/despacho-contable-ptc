a7e70e37fc5241db4ffb5d8a735b90772e810bbf
DarkKingpro10 <jesusgogeta@outlook.com>
2022-09-27 17:21:05 -0600

    Preparando para test

--- api/helpers/database.php
+++ api/helpers/database.php
@@ -16,7 +16,7 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'despEsquivel';
+        $database = 'dbdespcontesquivel';
         $username = 'postgres';
         $password = '1303';
 
