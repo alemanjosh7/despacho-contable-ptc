cbc872bb3ac339c7e1ce8cbb413238acad9e3ddc
DarkKingpro10 <84478517+DarkKingpro10@users.noreply.github.com>
2023-07-10 14:38:16 -0600

    Update enviarCorreoI.php

--- api/enviarCorreoI.php
+++ api/enviarCorreoI.php
@@ -148,7 +148,7 @@ class EnvioC
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'despcontableesquivel@gmail.com';                     //SMTP username
+            $mail->Username   = '';                     //SMTP username
             $mail->Password   = 'uzviadjrgvklbecp';                                   //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
